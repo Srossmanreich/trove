8063bf3c9933c3da46ff6b48864ad5003079449d
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-08-07 14:40:35 -0500

    Fix ES6 problem on safari

--- public/app.js
+++ public/app.js
@@ -1,3 +1,7 @@
+"use strict";
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var app = new Vue({
   el: "body",
   data: {
@@ -10,43 +14,75 @@ var app = new Vue({
     }
   },
   methods: {
-    onSignupUser(ev){
-      this.$http.post("/api/users", this.signup).then(output => {
-          if (output.data.success) {
-            this.signupSuccess = true;
-            localStorage.setItem("token", output.data.token);
-            localStorage.setItem("user", JSON.stringify(output.data.user));
-            window.location.href = "/thankyou";
-          }
-          else {
-            this.signupError = Object.assign({}, ...output.data.errors)
-          }
-      })
+    onSignupUser: function onSignupUser(ev) {
+      var _this = this;
+
+      this.$http.post("/api/users", this.signup).then(function (output) {
+        if (output.data.success) {
+          _this.signupSuccess = true;
+          localStorage.setItem("token", output.data.token);
+          localStorage.setItem("user", JSON.stringify(output.data.user));
+          window.location.href = "/thankyou";
+        } else {
+          _this.signupError = Object.assign.apply(Object, [{}].concat(_toConsumableArray(output.data.errors)));
+        }
+      });
     }
   }
-})
+});
 
+
+// ES6 below
 
 // var app = new Vue({
-  
-//   el: '#app',
-  
+//   el: "body",
 //   data: {
-//     message: 'Hello world'
+//     signup: {
+//       email: null
+//     },
+//     signupSuccess: false,
+//     signupError: {
+//       email: null
+//     }
 //   },
-  
-//   created: function(){
-//   },
-  
 //   methods: {
-//    onClick: ev => {
-//      fetch('/test')
-//        .then(output => output.json())
-//        .then(output =>{
-//          output.test
-//          app.message = output.test
-//        })
-//    }
+//     onSignupUser(ev){
+//       this.$http.post("/api/users", this.signup).then(output => {
+//           if (output.data.success) {
+//             this.signupSuccess = true;
+//             localStorage.setItem("token", output.data.token);
+//             localStorage.setItem("user", JSON.stringify(output.data.user));
+//             window.location.href = "/thankyou";
+//           }
+//           else {
+//             this.signupError = Object.assign({}, ...output.data.errors)
+//           }
+//       })
+//     }
 //   }
-
 // })
+
+
+// // var app = new Vue({
+  
+// //   el: '#app',
+  
+// //   data: {
+// //     message: 'Hello world'
+// //   },
+  
+// //   created: function(){
+// //   },
+  
+// //   methods: {
+// //    onClick: ev => {
+// //      fetch('/test')
+// //        .then(output => output.json())
+// //        .then(output =>{
+// //          output.test
+// //          app.message = output.test
+// //        })
+// //    }
+// //   }
+
+// // })
