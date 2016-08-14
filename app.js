5eded4899066f844535c45bc08b237745422e050
Ryan Paul <seg@phault.net>
2016-08-14 03:05:30 +0000

    Cleaned up codebase

--- app.js
+++ app.js
@@ -1,45 +1,31 @@
 
 const app = require("koa")();
 const router = require("koa-router")();
-const jwt = require("koa-jwt");
-const r = require("rethinkdbdash")({db: "trove"});
+const rethinkdb = require("rethinkdbdash")
 
 const config = require("./config");
+const r = rethinkdb(config.database);
 
 app.use(require("koa-bodyparser")());
 require("koa-validate")(app);
-
-// router.get("/test", function*() {
-// 	this.body = yield r.db("rethinkdb").table("stats");
-// });
 
 router.post("/api/users", function*() {
 	this.checkBody('email').isEmail("Please enter a real email");
 
 	if (this.errors) {
-        this.body = {success: false, errors: this.errors};
-        return;
-    }
+    this.body = {success: false, errors: this.errors};
+    return;
+	}
 
 	let {email} = this.request.body;
-	let newUser = {
-		email: email
-	};
+	let newUser = {email: email};
 
-	let data = yield r.table("users")
-	                  .insert(newUser, {returnChanges: true})("changes")("new_val");
+	let data = yield r.table("users").insert(newUser,
+		{returnChanges: true})("changes")("new_val");
 
-	this.body = {
-		success: true,
-		user: data,
-		token: jwt.sign(email, config.jwtSecret)
-	};
+	this.body = {success: true, user: data};
 });
 
-// router.get("/api/test", jwt({secret: config.jwtSecret}), function*() {
-// 	this.body = {success: "You passed the correct auth header"};
-// });
-
-app.use(require('koa-static')(__dirname + "/public"));
+app.use(require('koa-static')(`${__dirname}/public`));
 app.use(router.routes());
-app.listen(80);
+app.listen(config.port);
